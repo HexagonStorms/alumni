298fc2d456d280b310d10bfb132824985725d64a
Josue Plaza <plazajosue2@gmail.com>
2018-04-02 16:13:28 -0500

    - created models - added jumbotron - added student directory

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Student;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        return view('home');
+        $students = Student::get();
+        $data = [
+            'students' => $students
+        ];
+        return view('home')->with($data);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -17,15 +17,16 @@
 <body>
 
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#"><img src="/img/VTA-logo.png" width="100px" alt="Valley Technical Academy Logo" title="Valley Technical Academy" /></a>
+        <a class="navbar-brand" href="#"><img src="/img/VTA-logo.png" width="175px" alt="Valley Technical Academy Logo" title="Valley Technical Academy" /></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav ml-auto">
+                {{--
                 <li class="nav-item">
                     <a class="nav-link" href="#">Projects</a>
-                </li>
+                </li> --}}
                 <li class="nav-item">
                     <a class="nav-link" href="#">Alumni</a>
                 </li>
@@ -38,9 +39,22 @@
 
     <div class="container-fluid">
         <div class="row">
-            <div class="col">
-                <h1>Hello world</h1>
+            <div class="col jumbotron">
             </div>
+        </div>
+        <div class="row student-directory">
+            @foreach($students as $student)
+                <div class="col-4">
+                    <div class="card mb-3">
+                        <img class="card-img-top" src="{{ $student->image }}" alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $student->first_name }} {{ $student->last_name }}</h5>
+                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
         </div>
     </div>
 
